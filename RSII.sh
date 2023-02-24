e52189e56abab49c0a69bc73afcd56a861d65adf
TheKernalBlog <86623542+TheKernalBlog@users.noreply.github.com>
2023-02-24 22:37:35 +0000

    Add files via upload

--- RSII.sh
+++ RSII.sh
@@ -4,23 +4,47 @@ GREEN='\033[0;32m'
 RED='\e[1;31m'
 NOPE='\033[0m'
 
-sudo apt install fonts-firacode feh picom arandr xfce4-terminal thunar i3 git zathura
+echo -e " $GREEN What distro are you using Ubuntu, Fedora, or Arch. (U/F/A) $NOPE "
+read DISTRO 
+
+if [[ $DISTRO == "U" || $DISTRO == "u" ]] 
+then 
+
+	sudo apt install fonts-firacode feh picom arandr xfce4-terminal thunar i3 git zathura curl
+
+fi
+
+if [[ $DISTRO == "F" || $DISTRO == "f" ]]
+then 
+	sudo dnf install fira-code-fonts feh picom arandr xfce4-terminal thunar i3 git zathura curl
+fi 
+
+if [[ $DISTRO == "A" || $DISTRO == "a" ]] 
+then 
+	sudo pacman -S ttf-fira-code feh picom arandr xfce4-terminal thunar i3-wm git zathura curl 
+fi 
 
 git clone https://github.com/TheKernalBlog/ReasonableSystemInstall-Inator
 
-mv ~/ReasonableSystemInstall-Inator/alaska.jpg ~/Pictures/
+sudo rm -Rf ~/.vim/
+sudo rm -Rf ~/.config/i3/
+sudo rm -Rf ~/.config/i3status/ 
+
+sudo mv ~/ReasonableSystemInstall-Inator/alaska.jpg ~/Pictures/
 
 mkdir ~/.vim/
+mkdir ~/.vim/autoload/ 
 
-mv ~/ReasonableSystemInstall-Inator/vimrc ~/.vimrc
+sudo mv $HOME/ReasonableSystemInstall-Inator/vimrc ~/.vimrc
 
-mv ~/ReasonableSystemInstall-Inator/autoload/ ~/.vim/
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-mv ~/ReasonableSystemInstall-Inator/i3/ ~/.config/
+sudo mv ~/ReasonableSystemInstall-Inator/i3/ ~/.config/
 
-mv ~/ReasonableSystemInstall-Inator/i3status/ ~/.config/
+sudo mv ~/ReasonableSystemInstall-Inator/i3status/ ~/.config/
 
-mv ~/ReasonableSystemInstall-Inator/vimrc ~/.vimrc
+sudo mv ~/ReasonableSystemInstall-Inator/vimrc ~/.vimrc
 
 echo -e " $GREEN Would you like to use a GUI to configure multiple monitors? (y/n) $NOPE " 
 read RANDR
@@ -53,6 +77,7 @@ if [[ $GUI == "y" || $GUI == "Y" || $GUI == "yes" || $GUI == "Yes" ]]
 then
 
 echo -e " $GREEN You're done! Read more about your new system! $NOPE "
+
 zathura ~/ReasonableSystemInstall-Inator/GUIDE.pdf 
 
 else 
